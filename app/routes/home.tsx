92e290320da7a9bdebcadb3c6ce74e3982e4dc7a
bharathkdev <bharathaskkarur46@gmail.com>
2026-02-15 18:17:08 +0530

    feat(home): Implemented home page UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,9 @@
 import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
+import Button from "components/ui/Button";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -12,7 +14,92 @@ export default function Home() {
   return (
     <div className="home">
       <Navbar />
-      <h1 className="text-3xl text-indigo-700 font-extrabold">Welcome to React Router!</h1>
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+
+          <p>Introducing Planora AI 2.0</p>
+        </div>
+
+        <h1>
+          Create stunning interiors at speed of imagination with Planora AI
+        </h1>
+
+        <p className="subtitle">
+          Planora is a next-generation AI design platform for faster visualization and rendering of architectural ideas.
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building
+            <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, formats up to 10MB</p>
+            </div>
+
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Your latest work and shared community projects, all in one place.</p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Living Room" />
+
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+
+              <div className="card-body">
+                <div>
+                  <h3>Project New York</h3>
+
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>
+                      {new Date("01.01.2027").toLocaleDateString()}
+                    </span>
+                    <span>By Bharath</span>
+                  </div>
+                </div>
+
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
